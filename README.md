33cadc7abbeaf3fe2a24e712cb56b5435389a52e
高ping战士 <31578556+monSteRhhe@users.noreply.github.com>
2021-09-21 10:39:41 +0800

    upload

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # bilibili-dynamic-del
-B站抽奖动态自动删除
+用来删除B站已开奖的转发抽奖动态和“源动态已被删除”的动态。
+
+复制到 `F12→Console` 回车使用
+
+目前用起来可能还是会有各种各样的问题。
+
+
+
+------
+
+无差别删除转发的互动抽奖动态和“源动态已被删除”的动态（[来源](https://gwliang.com/2020/03/12/bilibili-del-Luck-draw-dynamic)）：
+
+```javascript
+var needDelDeled = true,
+    delay = 1000,
+    scrolls = 800;
+var w = '', d = '', r = 0;
+function getLuckyDraw() {
+    w.css("background-color", "#f1c40f");
+    w = w.parents(".card");
+    w.css("background-color", "#2ecc71");
+    w[w.length - 1].querySelectorAll(".child-button")[1].click();
+    setTimeout(clickDel, delay);
+}
+function getDel() {
+    d.css("background-color", "#8e44ad");
+    d = d.parents(".card");
+    d.css("background-color", "#2ecc71");
+    d[d.length - 1].querySelectorAll(".child-button")[1].click();
+    setTimeout(clickDel, delay);
+}
+function clickDel() {
+    //点删除
+    $(".popup-content-ctnr")[$(".popup-content-ctnr").length - 2].querySelector(".bl-button").click(); // 点确定
+    r += scrolls;
+    $('html, body').animate({ scrollTop: r }, 30);
+    $(".fold-text").click()
+    $(".expand-btn").click();
+    w = $(".main-content").find('span[click-title="抽奖详情"]');
+    d = $(".main-content").find('.deleted-text');
+    if (d.length && needDelDeled) setTimeout(getDel, delay);
+    else setTimeout(getLuckyDraw, delay);
+}
+if (/dynamic/.test(window.location.href) && confirm("是不是要删除抽奖动态")) {
+    r += scrolls;
+    $('html, body').animate({ scrollTop: r }, 30);
+    $(".fold-text").click()
+    $(".expand-btn").click();
+    w = $(".main-content").find('span[click-title="抽奖详情"]');//*互动抽奖内容定位
+    d = $(".main-content").find('.deleted-text');   //*已删除内容定位
+    if (d.length && needDelDeled) setTimeout(getDel, delay);
+    else setTimeout(getLuckyDraw, delay);
+}
+```
+
---
+++ autodel.js
@@ -0,0 +1,99 @@
+var needDelDeled = true, // 是否删除"源动态已被删除"的转发动态
+    delay = 1000; // 延迟时间
+
+var w = '', d = '';
+
+
+/* 标记d w */
+function mark(){
+    w = $(".main-content").find('span[click-title="抽奖详情"]'); // 互动抽奖内容定位
+    d = $(".main-content").find('.deleted-text');   // 已删除内容定位
+    if (d.length && needDelDeled) setTimeout(getDel, delay);
+    else setTimeout(filterW, delay);
+}
+
+
+/* 到底部 */
+function toEnd(){
+    window.scrollTo(0, document.documentElement.scrollHeight);
+    let end = document.getElementsByClassName('no-more')[0];
+    if(!end) setTimeout(toEnd, delay);
+    else {
+        $(".fold-text").click();
+        $(".expand-btn").click();
+        mark();
+    }
+}
+
+
+/* 筛选w */
+function filterW(){
+    w[w.length - 1].click();
+
+    var iframe = document.getElementsByTagName("iframe")[1];
+    iframe.onload = function(){
+        if(iframe.contentWindow.document.getElementsByClassName("countdown")[0]) {
+            $(".close-button").click();
+            $(w[w.length - 1]).attr('click-title', 'pass');
+
+            // 标记跳过的动态
+            wpass = $(".main-content").find('span[click-title="pass"]');
+            wpass = wpass.parents(".card");
+            wpass.css("background-color", "#bdc9bf");
+
+            setTimeout(mark, delay);
+        }else {
+            $(".close-button").click();
+            setTimeout(getLuckyDraw, delay);
+        }
+    }
+}
+
+
+/* 删除转发抽奖动态 */
+function getLuckyDraw(){
+    w.css("background-color", "#f1c40f");
+    w = w.parents(".card");
+    w.css("background-color", "#2ecc71");
+    console.log(w[0])
+    w[0].querySelectorAll(".child-button")[1].click();
+    setTimeout(clickDel, delay);
+}
+
+
+/* 删除"源动态已被删除"的转发动态 */
+function getDel(){
+    d.css("background-color", "#8e44ad");
+    d = d.parents(".card");
+    d.css("background-color", "#2ecc71");
+    d[d.length - 1].querySelectorAll(".child-button")[1].click();
+    setTimeout(clickDel, delay);
+}
+
+
+/* 点删除 */
+function clickDel(){
+    // 点确定
+    $(".popup-content-ctnr")[$(".popup-content-ctnr").length - 2].querySelector(".bl-button").click();
+    setTimeout(mark, delay);
+}
+
+
+/* 判断是否为当前账号 */
+function accountCheck(){
+    var mycookiearr = document.cookie.split(';');
+    for(var i = 0; i < mycookiearr.length; i++){
+        var ckstr = mycookiearr[i].split('=')[0];
+        var cknum = mycookiearr[i].split('=')[1];
+        if(ckstr == ' DedeUserID') var myuid = cknum; // 获取uid
+    };
+    var urlpath = window.location.pathname;
+    if (urlpath.split('/')[1] == myuid) return true;
+    else return false;
+}
+
+
+/* 正则匹配动态页面 & 删除确定 */
+if (/dynamic/.test(window.location.href) && accountCheck() && confirm("确定要删除抽奖动态?")) {
+    setTimeout(toEnd, delay);
+}
