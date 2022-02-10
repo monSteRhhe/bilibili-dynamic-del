896738500433fc4964bcfb94bfea68ba4127e67e
高ping战士 <31578556+monSteRhhe@users.noreply.github.com>
2022-02-11 01:17:49 +0800

    update
    应该算是大修改？

--- autodel.js
+++ autodel.js
@@ -1,105 +1,108 @@
-var needDelDeled = true, // 是否删除'源动态已被删除'的转发动态
-    delay = 800; // 延迟时间
+'use strict';
 
-var w = '', d = '';
+var needDeld = true, // 是否删除'源动态已被删除'的转发动态
+    delay = 1000, // 操作延迟时间
+    reload_delay = 5000; // 加载动态的时间（不知道会不会用得上
 
 
-/* 标记d w */
-function mark(){
-    w = $('.main-content').find('span[click-title="抽奖详情"]'); // 互动抽奖内容定位
-    d = $('.main-content').find('.deleted-text');   // 已删除内容定位
-    if (d.length && needDelDeled) setTimeout(getDel, delay);
-    else setTimeout(filterW, delay);
+function start() {
+    $('.fold-text, .expand-btn').click(); // 展开动态
+    $('img, div.img-content, a.up-info-avatar, div.shop-item').remove(); // 删除加载的图片
+    process();
 }
 
 
-/* 到底部 */
-function toEnd(){
-    window.scrollTo(0, document.documentElement.scrollHeight);
-    // 删除加载的图片
-    $('img').remove();
-    $('div.img-content').remove();
-    $('a.up-info-avatar').remove();
-    $('div.shop-item').remove();
+/* d/w */
+function process(){
+    if($(document).scrollTop() == 0) $(document).scrollTop(150); // 向下滚动一段距离
 
+    var w = new Array(); // 互动抽奖
+    var d = new Array(); // 源动态已删除
 
-    let end = document.getElementsByClassName('no-more')[0];
-    if(!end) setTimeout(toEnd, delay);
-    else {
-        $('.fold-text').click();
-        $('.expand-btn').click();
-        mark();
-    }
+    $('div.card').each(function() {
+        if ($(this).find('span[click-title="抽奖详情"]').length > 0) w.push($(this).find('span[click-title="抽奖详情"]'));
+        else if ($(this).find('.deleted-text').length > 0) d.push($(this).find('.deleted-text'));
+        else $(this).remove(); // 移除其他动态
+    })
+
+    if (d.length && needDeld) setTimeout(getDel(d), delay); // 处理源动态已删除的转发动态
+    else setTimeout(filterW(w), delay); // 处理转发互动抽奖动态
 }
 
 
 /* 筛选w */
-function filterW(){
-    w[w.length - 1].click();
+function filterW(w){
+    if(w.length > 0) {
+        w[0].click();
 
-    var iframe = document.getElementsByTagName('iframe')[1];
-    iframe.onload = function(){
-        if(iframe.contentWindow.document.getElementsByClassName('countdown')[0]) {
-            $('.close-button').click();
-            $(w[w.length - 1]).attr('click-title', 'pass');
-
-            // 标记跳过的动态
-            wpass = $('.main-content').find('span[click-title="pass"]');
-            wpass = wpass.parents('.card');
-            wpass.css('background-color', '#bdc9bf');
-
-            setTimeout(mark, delay);
-        }else {
-            $('.close-button').click();
-            setTimeout(getLuckyDraw, delay);
+        var iframe = document.getElementsByTagName('iframe')[1];
+        iframe.onload = function(){
+            if(iframe.contentWindow.document.getElementsByClassName('countdown')[0] != null) {
+                destroyIframe(iframe);
+                setTimeout(function() {
+                    $('div.close-button').click();
+                    $(w[0]).parents('.card').remove(); // 移除未开奖的转发抽奖动态
+                }, delay);
+                setTimeout(process, delay);
+            }
+            else {
+                destroyIframe(iframe);
+                setTimeout(function() {$('div.close-button').click();}, delay);
+                setTimeout(getLuckyDraw(w), delay); // 删除已开奖的转发抽奖动态
+            }
         }
     }
+    else if($('div.content > div.div-load-more > div.load-more').length > 0) setTimeout(start, reload_delay);
+    else if($('div.content > div.div-load-more > div.no-more').length > 0) return;
+}
+
+
+/* 释放iframe内存 */
+function destroyIframe(iframe) {
+    iframe.contentWindow.document.close();
+    iframe.contentWindow.document.clear();
+    iframe.src = 'about:blank';
+    iframe.remove();
 }
 
 
 /* 删除转发抽奖动态 */
-function getLuckyDraw(){
-    w.css('background-color', '#f1c40f');
-    w = w.parents('.card');
-    w.css('background-color', '#2ecc71');
-    w[0].querySelectorAll('.child-button')[1].click();
+function getLuckyDraw(w){
+    $(w[0]).css('background-color', '#f1c40f');
+    $(w[0]).parents('.card').css('background-color', '#2ecc71');
+    
+    $(w[0]).parents('.card').find('.child-button')[1].click();
     setTimeout(clickDel, delay);
 }
 
 
 /* 删除'源动态已被删除'的转发动态 */
-function getDel(){
-    d.css('background-color', '#8e44ad');
-    d = d.parents('.card');
-    d.css('background-color', '#2ecc71');
-    d[d.length - 1].querySelectorAll('.child-button')[1].click();
+function getDel(d){
+    $(d[0]).css('background-color', '#8e44ad');
+    $(d[0]).parents('.card').css('background-color', '#2ecc71');
+
+    $(d[0]).parents('.card').find('.child-button')[1].click();
     setTimeout(clickDel, delay);
 }
 
 
 /* 点删除 */
 function clickDel(){
-    // 点确定
-    $('.popup-content-ctnr')[$('.popup-content-ctnr').length - 2].querySelector('.bl-button').click();
-    setTimeout(mark, delay);
+    $('div.popup-content-ctnr').find('.bl-button--primary').click(); // 点确定
+    setTimeout(process, delay);
 }
 
 
 /* 判断是否为当前账号 */
 function accountCheck(){
-    var mycookiearr = document.cookie.split(';');
-    for(var i = 0; i < mycookiearr.length; i++){
-        var ckstr = mycookiearr[i].split('=')[0];
-        var cknum = mycookiearr[i].split('=')[1];
-        if(ckstr == ' DedeUserID') var myuid = cknum; // 获取uid
-    };
-    var urlpath = window.location.pathname;
-    if (urlpath.split('/')[1] == myuid) return true;
-    else return false;
+    var cookieArr = document.cookie.split(';');
+    for(var i = 0; i < cookieArr.length; i++) {
+        var ckstr = cookieArr[i].split('=')[0];
+        var cknum = cookieArr[i].split('=')[1];
+        if(ckstr == ' DedeUserID') return window.location.pathname.split('/')[1] == cknum;
+    }
 }
 
 
-/* 正则匹配动态页面 & 删除确定 */
-if (/dynamic/.test(window.location.href) && accountCheck() && confirm('确定要删除抽奖动态?')) {
-    setTimeout(toEnd, delay);
-}
+/* 匹配页面/UID & 删除确认 */
+if (window.location.href.indexOf('dynamic') != -1 && accountCheck() && confirm('确定要删除抽奖动态?')) setTimeout(start, delay);
